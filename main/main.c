b3ff7815ae5aba6f7ddae019f1b6332748ee152a
Juan <juanpablovillamilmiranda@gmail.com>
2026-04-19 21:57:42 -0500

    Final

--- main/main.c
+++ main/main.c
@@ -38,10 +38,7 @@
 
 #define SAMPLE_PERIOD_US 20000
 
-//-----------------------------------
-// Variables
-//-----------------------------------
-
+//Variables
 static volatile int value1=0;
 static volatile int value2=0;
 static volatile int value3=0;
@@ -52,114 +49,100 @@ static int act=0;
 static volatile int64_t antireizq=0;
 static volatile int64_t antireder=0;
 
-//-----------------------------------
-// 7 segmentos
-//-----------------------------------
-
+//Display 7seg
 const uint8_t digitos[10]={
-0b00111111,
-0b00000110,
-0b01011011,
-0b01001111,
-0b01100110,
-0b01101101,
-0b01111101,
-0b00000111,
-0b01111111,
-0b01101111
+    0b00111111,
+    0b00000110,
+    0b01011011,
+    0b01001111,
+    0b01100110,
+    0b01101101,
+    0b01111101,
+    0b00000111,
+    0b01111111,
+    0b01101111
 };
 
-void mostrar_segmentos(uint8_t patron)
-{
-gpio_set_level(A,!((patron>>0)&1));
-gpio_set_level(B,!((patron>>1)&1));
-gpio_set_level(C,!((patron>>2)&1));
-gpio_set_level(D,!((patron>>3)&1));
-gpio_set_level(E,!((patron>>4)&1));
-gpio_set_level(F,!((patron>>5)&1));
-gpio_set_level(G,!((patron>>6)&1));
+void mostrar_segmentos(uint8_t patron){
+    gpio_set_level(A,!((patron>>0)&1));
+    gpio_set_level(B,!((patron>>1)&1));
+    gpio_set_level(C,!((patron>>2)&1));
+    gpio_set_level(D,!((patron>>3)&1));
+    gpio_set_level(E,!((patron>>4)&1));
+    gpio_set_level(F,!((patron>>5)&1));
+    gpio_set_level(G,!((patron>>6)&1));
 }
 
-void multiplexar_display(int n1,int n2,int n3)
-{
-gpio_set_level(DIG1,1);
-gpio_set_level(DIG2,1);
-gpio_set_level(DIG3,1);
+void multiplexar_display(int n1,int n2,int n3){
+    gpio_set_level(DIG1,1);
+    gpio_set_level(DIG2,1);
+    gpio_set_level(DIG3,1);
 
-mostrar_segmentos(digitos[n1]);
-gpio_set_level(DIG1,0);
-vTaskDelay(pdMS_TO_TICKS(5));
+    mostrar_segmentos(digitos[n1]);
+    gpio_set_level(DIG1,0);
+    vTaskDelay(pdMS_TO_TICKS(5));
 
-gpio_set_level(DIG1,1);
-gpio_set_level(DIG2,1);
-gpio_set_level(DIG3,1);
+    gpio_set_level(DIG1,1);
+    gpio_set_level(DIG2,1);
+    gpio_set_level(DIG3,1);
 
-mostrar_segmentos(digitos[n2]);
-gpio_set_level(DIG2,0);
-vTaskDelay(pdMS_TO_TICKS(5));
+    mostrar_segmentos(digitos[n2]);
+    gpio_set_level(DIG2,0);
+    vTaskDelay(pdMS_TO_TICKS(5));
 
-gpio_set_level(DIG1,1);
-gpio_set_level(DIG2,1);
-gpio_set_level(DIG3,1);
+    gpio_set_level(DIG1,1);
+    gpio_set_level(DIG2,1);
+    gpio_set_level(DIG3,1);
 
-mostrar_segmentos(digitos[n3]);
-gpio_set_level(DIG3,0);
-vTaskDelay(pdMS_TO_TICKS(5));
+    mostrar_segmentos(digitos[n3]);
+    gpio_set_level(DIG3,0);
+    vTaskDelay(pdMS_TO_TICKS(5));
 }
 
-//-----------------------------------
-// Interrupciones botones
-//-----------------------------------
+//Interrupciones
 
-static void IRAM_ATTR izquierda(void *arg)
-{
-int64_t now=esp_timer_get_time();
+static void IRAM_ATTR izquierda(void *arg){
+    int64_t now=esp_timer_get_time();
 
-if(now-antireizq>200000)
-{
-direccion=1;
-antireizq=now;
-}
+    if(now-antireizq>200000)
+    {
+        direccion=1;
+        antireizq=now;
+    }
 }
 
-static void IRAM_ATTR derecha(void *arg)
-{
-int64_t now=esp_timer_get_time();
+static void IRAM_ATTR derecha(void *arg){
+    int64_t now=esp_timer_get_time();
 
-if(now-antireder>200000)
-{
-direccion=2;
-antireder=now;
-}
+    if(now-antireder>200000)    {
+        direccion=2;
+        antireder=now;
+    }
 }
 
-//-----------------------------------
-// Main
-//-----------------------------------
-
-void app_main(void)
-{
+//Programa princpioal
+void app_main(void){
 
 // GPIO salida
 
 gpio_config_t leds={
-.pin_bit_mask=
-(1ULL<<A)|
-(1ULL<<B)|
-(1ULL<<C)|
-(1ULL<<D)|
-(1ULL<<E)|
-(1ULL<<F)|
-(1ULL<<G)|
-(1ULL<<DIG1)|
-(1ULL<<DIG2)|
-(1ULL<<DIG3)|
-(1ULL<<LED_RED)|
-(1ULL<<LED_GREEN)|
-(1ULL<<Left)|
-(1ULL<<right),
+    .pin_bit_mask=
+    (1ULL<<A)|
+    (1ULL<<B)|
+    (1ULL<<C)|
+    (1ULL<<D)|
+    (1ULL<<E)|
+    (1ULL<<F)|
+    (1ULL<<G)|
+    (1ULL<<DIG1)|
+    (1ULL<<DIG2)|
+    (1ULL<<DIG3)|
+    (1ULL<<LED_RED)|
+    (1ULL<<LED_GREEN)|
+    (1ULL<<Left)|
+    (1ULL<<right),
 
-.mode=GPIO_MODE_OUTPUT
+    .mode=GPIO_MODE_OUTPUT
 };
 
 ESP_ERROR_CHECK(gpio_config(&leds));
@@ -167,13 +150,13 @@ ESP_ERROR_CHECK(gpio_config(&leds));
 // GPIO entradas
 
 gpio_config_t inputs={
-.pin_bit_mask=
-(1ULL<<Left_button)|
-(1ULL<<right_button),
+    .pin_bit_mask=
+    (1ULL<<Left_button)|
+    (1ULL<<right_button),
 
-.mode=GPIO_MODE_INPUT,
-.pull_up_en=GPIO_PULLUP_ENABLE,
-.intr_type=GPIO_INTR_NEGEDGE
+    .mode=GPIO_MODE_INPUT,
+    .pull_up_en=GPIO_PULLUP_ENABLE,
+    .intr_type=GPIO_INTR_NEGEDGE
 };
 
 ESP_ERROR_CHECK(gpio_config(&inputs));
@@ -183,16 +166,13 @@ gpio_install_isr_service(0);
 gpio_isr_handler_add(Left_button,izquierda,NULL);
 gpio_isr_handler_add(right_button,derecha,NULL);
 
-//-----------------------------------
-// GPTIMER NUEVO
-//-----------------------------------
-
+//Coonfig Timers (Versión 6.0 de ESP-IDF)
 gptimer_handle_t adc_timer=NULL;
 
 gptimer_config_t timer_cfg={
-.clk_src=GPTIMER_CLK_SRC_DEFAULT,
-.direction=GPTIMER_COUNT_UP,
-.resolution_hz=1000000
+    .clk_src=GPTIMER_CLK_SRC_DEFAULT,
+    .direction=GPTIMER_COUNT_UP,
+    .resolution_hz=1000000
 };
 
 ESP_ERROR_CHECK(
@@ -213,10 +193,7 @@ gptimer_start(adc_timer)
 
 uint64_t timer_value=0;
 
-//-----------------------------------
-// ADC
-//-----------------------------------
-
+//Lectura de datos potenciometro
 adc_oneshot_unit_handle_t adc1_handle;
 
 adc_oneshot_unit_init_cfg_t init_config={
@@ -245,10 +222,7 @@ ADC_CHANNEL_6,
 
 int adc_raw=0;
 
-//-----------------------------------
-// PWM
-//-----------------------------------
-
+//Pwm
 ledc_timer_config_t ledc_timer={
 .speed_mode=LEDC_LOW_SPEED_MODE,
 .timer_num=LEDC_TIMER_0,
@@ -268,10 +242,6 @@ ledc_channel_config_t channel={
 };
 
 ledc_channel_config(&channel);
-
-//-----------------------------------
-// LOOP
-//-----------------------------------
 
 while(1)
 {
